2493413bcab9ce228eb3db146caa589c756b8fdc
Ricardo Mora <rurimoca@gmail.com>
2024-04-16 03:57:56 -0600

    Corrección con retorno de carro

--- scripts/compile_and_run_server.sh
+++ scripts/compile_and_run_server.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-# Variables para los nombres de los archivos de salida
-SERVER_BIN="./server/server"
-CLIENT_BIN="./client/client"
+SERVER_SRC="./../server/server.c"
+CLIENT_SRC="./../client/client.c"
+SERVER_BIN="./../server/server"
+CLIENT_BIN="./../client/client"
 
 # Eliminar el archivo binario del servidor si existe
 if [ -f "$SERVER_BIN" ]; then
-    rm "$SERVER_BIN"
+  rm "$SERVER_BIN"
 fi
 
 # Compilar el programa servidor
-gcc ./../server/server.c -o "$SERVER_BIN"
+gcc "$SERVER_SRC" -o "$SERVER_BIN"
 if [ $? -ne 0 ]; then
-    echo "Err./clienteor al compilar el programa servidor."
-    exit 1
+  echo "Error al compilar el programa servidor."
+  exit 1
 fi
 
 # Compilar el programa cliente
-gcc ./../client/client.c -o "$CLIENT_BIN"
+gcc "$CLIENT_SRC" -o "$CLIENT_BIN"
 if [ $? -ne 0 ]; then
-    echo "Error al compilar el programa cliente."
-    exit 1
+  echo "Error al compilar el programa cliente."
+  exit 1
 fi
 
 # Limpiar la pantalla
 clear
 
-# Ejecutar el programa servidor solo si la compilación fue exitosa
-if [ -x "$SERVER_BIN" ]; then
-    "$SERVER_BIN"
+# Verificar la existencia y ejecutar el programa servidor
+if [ -f "$SERVER_BIN" ]; then
+  "$SERVER_BIN"
 else
-    echo "No se puede ejecutar el programa servidor debido a un error de compilación."
+  echo "Error: No se pudo crear el programa servidor correctamente."
 fi
-
